3e0d706e44550d42977656a0946c367bc7a5db94
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-01-22 21:40:59 -0300

    Add 'AddTraining' tab

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,19 @@
 import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 export default function Layout () {
   return (
     <Tabs>
-      <Tabs.Screen name='index' options={{ headerShown: false, title: 'Home' }} />
+      <Tabs.Screen name='add-training/index' options={{
+        headerShown: false,
+        tabBarLabel: 'Add Training',
+        tabBarIcon: ({ color, size }) => <Ionicons name='add' color={color} size={size} />
+      }} />
+      <Tabs.Screen name='index' options={{
+        headerShown: false,
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color, size }) => <Ionicons name='home' color={color} size={size} />
+      }} />
     </Tabs>
   )
 }
--- app/add-training/index.tsx
+++ app/add-training/index.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native'
 
-export default function Home () {
+export default function AddTraining () {
   return (
     <View>
-      <Text>TESTEEEE INDEX</Text>
+      <Text>ADD TRAINING</Text>
     </View>
   )
 }
