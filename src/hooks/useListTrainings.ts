4db3c64936d01b9ef1471489ad4458935da46223
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-01-23 22:19:51 -0300

    Add useListTraining hook

---
+++ src/hooks/useListTrainings.ts
@@ -0,0 +1,39 @@
+import { useSQLiteContext } from 'expo-sqlite'
+import { useState } from 'react'
+
+export type TrainingType = {
+  id: number
+  name: string
+  description?: string | null
+}
+
+/**
+ * Hook used to list all existing training types.
+ * @returns The list of training types, loading status, and error.
+ */
+export function useListTrainings () {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<Error | null>(null)
+  const db = useSQLiteContext()
+
+  const listTrainings = async (): Promise<TrainingType[] | void> => {
+    if (!db) {
+      setError(new Error('An error occurred while getting the database.'))
+      return
+    }
+
+    const query = 'SELECT * FROM training_types'
+
+    setLoading(true)
+    try {
+      const response = await db.getAllAsync<TrainingType>(query)
+      return response
+    } catch (error) {
+      setError(error as Error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { listTrainings, loading, error }
+}
