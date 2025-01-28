4afc673f2e43dbc67e6a0ff6f68fee51c02007c0
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-01-28 20:53:13 -0300

    Add PaperProvider to root _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,22 +3,25 @@ import { Ionicons } from '@expo/vector-icons'
 import { SQLiteProvider } from 'expo-sqlite'
 import { initDatabase } from '../src/database/initDatabase'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import { PaperProvider } from 'react-native-paper'
 
 export default function Layout () {
   return (
-    <SQLiteProvider databaseName='sqlite.db' onInit={initDatabase}>
-      <Tabs>
-        <Tabs.Screen name='training-types' options={{
-          headerShown: false,
-          tabBarLabel: 'Training Types',
-          tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="dumbbell" size={size} color={color} />
-        }} />
-        <Tabs.Screen name='index' options={{
-          headerShown: false,
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => <Ionicons name='home' color={color} size={size} />
-        }} />
-      </Tabs>
-    </SQLiteProvider>
+    <PaperProvider>
+      <SQLiteProvider databaseName='sqlite.db' onInit={initDatabase}>
+        <Tabs>
+          <Tabs.Screen name='training-types' options={{
+            headerShown: false,
+            tabBarLabel: 'Training Types',
+            tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="dumbbell" size={size} color={color} />
+          }} />
+          <Tabs.Screen name='index' options={{
+            headerShown: false,
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => <Ionicons name='home' color={color} size={size} />
+          }} />
+        </Tabs>
+      </SQLiteProvider>
+    </PaperProvider>
   )
 }
