c52593159e3998385dd7671bc163ecead558ea84
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-01-30 01:17:55 -0300

    List training types on TrainingTypes page

--- app/training-types/index.tsx
+++ app/training-types/index.tsx
@@ -1,21 +1,36 @@
-import { GestureResponderEvent, StyleSheet, Text, View } from 'react-native'
-import { useCreateTraining } from '../../src/hooks'
-import { Button } from 'react-native-paper'
+import { Button, Text } from 'react-native-paper'
 import { Ionicons } from '@expo/vector-icons'
-import { Link, router } from 'expo-router'
+import { router, useFocusEffect } from 'expo-router'
+import { FlatList, View, StyleSheet } from 'react-native'
+import { useCallback, useState } from 'react'
+import { useListTrainings } from '../../src/hooks'
+import { TrainingType } from '../../src/types'
 
 export default function TrainingTypes () {
-  const handleCreateClick = () => {
-    router.push('/training-types/create-type')
-  }
+  const { listTrainings } = useListTrainings()
+  const [trainingTypes, setTrainingTypes] = useState<TrainingType[] | null>([])
+
+  useFocusEffect(
+    useCallback(() => {
+      const list = async () => {
+        const trainings = await listTrainings()
+        setTrainingTypes(trainings ?? [])
+      }
+      list()
+    }, [])
+  )
 
   return (
     <View style={styles.container}>
-      <Text>ADD TRAINIaaaNG</Text>
-      <Button mode='elevated' onPress={handleCreateClick}>
+      <Button mode='elevated' onPress={() => router.push('/training-types/create-type')}>
         <Ionicons name='add' color={'black'} />
-        Adicionar novo treino
+        Add new training
       </Button>
+
+      <FlatList
+        data={trainingTypes}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
     </View>
   )
 }
