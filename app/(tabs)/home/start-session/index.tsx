566a990a3122b9e083c5e28fc0eee2289eeb257d
GabrielNhoncanse <gabrielcnhoncanse@gmail.com>
2025-02-02 18:28:27 -0300

    Add trainingType picker to start-session page

--- app/(tabs)/home/start-session/index.tsx
+++ app/(tabs)/home/start-session/index.tsx
@@ -1,13 +1,44 @@
 import { View } from 'react-native'
 import { Text } from 'react-native-paper'
+import { Picker } from '@react-native-picker/picker'
+import { useCallback, useEffect, useState } from 'react'
+import { GetTrainingByIdResult, useGetTrainingById, useListTrainings } from '../../../../src/hooks'
+import { Exercise, TrainingType } from '../../../../src/types'
+import { useFocusEffect } from 'expo-router'
+import { ExercisesTable } from '../../../../src/components'
 
 export default function StartSession () {
+  const [trainingTypes, setTrainingTypes] = useState<TrainingType[]>([])
+  const [exercises, setExercises] = useState<Exercise[]>([])
+  const { listTrainings } = useListTrainings()
+  const { getTrainingById } = useGetTrainingById()
+
+  useFocusEffect(
+    useCallback(() => {
+      const list = async () => {
+        const trainings = await listTrainings()
+        setTrainingTypes(trainings ?? [])
+      }
+      list()
+    }, []))
+
+  const handleTrainingSelected = async (trainingId: number) => {
+    const trainingData = await getTrainingById(trainingId!) as GetTrainingByIdResult
+    setExercises(trainingData.exercises)
+  }
 
   return (
-    <View>
-      <Text>
-        AAAAAA
-      </Text>
+    <View style={{ padding: 20 }}>
+      <Picker
+        onValueChange={(selectedId: number) => handleTrainingSelected(selectedId)}>
+        {trainingTypes?.map((training) => (
+          <Picker.Item key={training.id} label={training.name} value={training.id} />
+        ))}
+      </Picker>
+
+      {exercises.length !== 0 && (
+        <ExercisesTable exercises={exercises} />
+      )}
     </View>
   )
 }
